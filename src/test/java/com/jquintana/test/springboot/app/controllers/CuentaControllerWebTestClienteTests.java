1408e8445977bcacf69be0d6cfc2ad851071b868
Juan Quintana <quintanajuanchy25@gmail.com>
2025-08-19 09:34:04 -0300

    add new methods of tests

--- src/test/java/com/jquintana/test/springboot/app/controllers/CuentaControllerWebTestClienteTests.java
+++ src/test/java/com/jquintana/test/springboot/app/controllers/CuentaControllerWebTestClienteTests.java
@@ -1,15 +1,17 @@
 package com.jquintana.test.springboot.app.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jquintana.test.springboot.app.models.Cuenta;
 import com.jquintana.test.springboot.app.models.TransaccionDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -20,6 +22,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class CuentaControllerWebTestClienteTests {
     @Autowired
@@ -33,6 +36,7 @@ class CuentaControllerWebTestClienteTests {
     }
 
     @Test
+    @Order(1)
     void testTransferir() throws Exception {
         // Given
         TransaccionDto dto = new TransaccionDto();
@@ -48,12 +52,23 @@ class CuentaControllerWebTestClienteTests {
         response.put("transaccion", dto);
 
         // When
-        client.post().uri("http://localhost:8080/api/cuentas/transferir")
+        client.post().uri("/api/cuentas/transferir")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(dto)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
+                .consumeWith(respuesta -> {
+                    try {
+                        JsonNode json = objectMapper.readTree(respuesta.getResponseBody());
+                        assertEquals("Transferencia realizada con exito", json.path("mensaje").asText());
+                        assertEquals(1L, json.path("transaccion").path("cuentaOrigenId").asLong());
+                        assertEquals(LocalDate.now().toString(), json.path("date").asText());
+                        assertEquals("100", json.path("transaccion").path("monto").asText());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .jsonPath("$.mensaje").isNotEmpty()
                 .jsonPath("$.mensaje").value(is("Transferencia realizada con exito"))
                 .jsonPath("$.mensaje").value(valor -> assertEquals("Transferencia realizada con exito", valor))
@@ -61,5 +76,41 @@ class CuentaControllerWebTestClienteTests {
                 .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
                 .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
                 .json(objectMapper.writeValueAsString(response));
+    }
+
+    @Test
+    @Order(2)
+    void testDetalle() throws Exception {
+        client.get().uri("/api/cuentas/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .consumeWith(response -> {
+                    try {
+                        JsonNode json = objectMapper.readTree(response.getResponseBody());
+                        assertEquals("Juan", json.path("persona").asText());
+                        assertEquals(1000, json.path("saldo").asLong());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .jsonPath("$.persona").isEqualTo("Juan")
+                .jsonPath("$.saldo").isEqualTo(1000);
+    }
+
+    @Test
+    @Order(3)
+    void testDetalle2() {
+        client.get().uri("/api/cuentas/2")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Cuenta.class)
+                .consumeWith(response -> {
+                        Cuenta cuenta = response.getResponseBody();
+                        assertEquals("Diego", cuenta.getPersona());
+                        assertEquals("2000.00", cuenta.getSaldo().toPlainString());
+                });
     }
 }
